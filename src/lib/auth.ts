7ab1b488261e229ede9a5cc7c54e9d216ca7a78d
am1se <shushko.art@gmail.com>
2026-07-23 18:55:37 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,9 @@
 import { hash, compare } from 'bcrypt-ts'
 
-// Хешируем пароль перед тем, как пихнуть его в базу
 export async function hashPassword(password: string) {
   return hash(password, 12)
 }
 
-// Сверяем введенный пароль с тем, что лежит в базе
 export async function verifyPassword(password: string, hash: string) {
   return compare(password, hash)
 }
