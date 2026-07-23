3d089bdf2fe14651344f168f2b0a4453d25f8a4e
am1se <shushko.art@gmail.com>
2026-07-23 17:49:45 +0300

    Update route.ts

--- src/app/api/plugin/kick/route.ts
+++ src/app/api/plugin/kick/route.ts
@@ -1,1 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server'
 
+export async function POST(request: NextRequest) {
+  if (request.headers.get('X-API-SECRET') !== process.env.API_SECRET) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
+  return NextResponse.json({ status: 'success' })
+}
