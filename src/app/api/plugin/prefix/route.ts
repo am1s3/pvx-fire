14ba4d78a1cb09d80b301278d083d893f62b1f75
am1se <shushko.art@gmail.com>
2026-07-23 22:55:58 +0300

    Update route.ts

--- src/app/api/plugin/prefix/route.ts
+++ src/app/api/plugin/prefix/route.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { supabaseAdmin } from '@/lib/supabase'
 
+export const runtime = 'edge';
+
 export async function POST(request: NextRequest) {
   if (request.headers.get('X-API-SECRET') !== process.env.API_SECRET) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
   }
   const { target, prefix, action } = await request.json()
+  
   await supabaseAdmin.from('users').update({ prefix: action === 'remove' ? '' : prefix }).ilike('mc_nick', target)
+  await supabaseAdmin.from('myths').update({ prefix: action === 'remove' ? '' : prefix }).ilike('mc_nick', target)
+  
   return NextResponse.json({ status: 'success' })
 }
